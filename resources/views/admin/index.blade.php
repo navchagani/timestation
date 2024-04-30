392e3292c681b891fb1b6c46e44d604f9f6ac1ec
Gmurtazak <murtazakasmanis@gmail.com>
2024-04-30 21:53:22 +0500

    colour tag
    - colour tag

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -330,7 +330,7 @@
                 var bodyData = '';
                 $.each(data.attendances, function (index, attendance) {
                     bodyData += "<tr>";
-                    bodyData += "<td>" + (attendance.status == 1 ? 'IN' : 'OUT') + "</td><td>" + attendance.name + "</td><td>" + (attendance.status == 1 ? 'IN' : 'OUT') + " at " + attendance.attendance_time + " On " + attendance.attendance_date + "</td>";
+                    bodyData += "<td>" + (attendance.status == 1 ? '<span class="badge badge-success badge-pill float-right">IN</span>' : '<span class="badge badge-danger badge-pill float-right">OUT</span>') + "</td><td>" + attendance.name + "</td><td>" + (attendance.status == 1 ? 'IN' : 'OUT') + " at " + attendance.attendance_time + " On " + attendance.attendance_date + "</td>";
                     bodyData += "</tr>";
                 });
                 $("#bodyData").html(bodyData);
