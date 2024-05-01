238b770e14e1b786b71a8deacedd213aa1544769
Gmurtazak <murtazakasmanis@gmail.com>
2024-05-01 21:17:30 +0500

    Update employee information
    1- Update employee information

--- resources/views/admin/employee.blade.php
+++ resources/views/admin/employee.blade.php
@@ -43,12 +43,13 @@
 
                                                     <thead>
                                                     <tr>
-                                                        <th data-priority="1">Employee ID</th>
+                                                        <th data-priority="1">Employee Pin</th>
+                                                        <th data-priority="5">Title</th>
                                                         <th data-priority="2">Name</th>
                                                         <th data-priority="3">Department</th>
-                                                        <th data-priority="5">Title</th>
-                                                        <th data-priority="4">Status</th>
-                                                        <th data-priority="6">QR</th>
+                                                        <th data-priority="4">Email</th>
+                                                        <th data-priority="4">Employee type</th>
+                                                        <th data-priority="6">Employee QR</th>
                                                        {{-- <th data-priority="7">Actions</th>--}}
 
                                                     </tr>
@@ -58,13 +59,13 @@
 
                                                         <tr>
                                                             <td>{{$employee->empid}}</td>
+                                                            <td>{{$employee->title}}</td>
                                                             <td>{{$employee->name}}</td>
                                                             <td>{{$employee->position}}</td>
-                                                            <td>{{$employee->title}}</td>
                                                             <td>{{$employee->email}}</td>
+                                                            <td>{{$employee->type}}</td>
                                                             <td>  {!! QrCode::size(100)->generate('{{$employee->empid}}') !!}</td>
                                                            {{-- <td>
-
                                                                 <a href="#edit{{$employee->name}}" data-toggle="modal" class="btn btn-success btn-sm edit btn-flat"><i class='fa fa-edit'></i> Edit</a>
                                                                 <a href="#delete{{$employee->name}}" data-toggle="modal" class="btn btn-danger btn-sm delete btn-flat"><i class='fa fa-trash'></i> Delete</a>
                                                             </td>--}}
