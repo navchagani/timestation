4abffce88b644898518e7c43943bd3d3401df579
Gmurtazak <murtazakasmanis@gmail.com>
2024-05-03 00:45:41 +0500

    update
    update

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -44,7 +44,7 @@ class EmployeeController extends Controller
                         "username" => $user->uname,
                         "email" => $user->email,
                         "location_id" => $user->lname,
-                        'employees'=> Employee::all()
+                        'employees'=> Employee::all()->first()
                     ]
                 ], 200);
             } else {
