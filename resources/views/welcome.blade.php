57278a4176bb440c9bf3b9bb211a824f25e198e4
Gmurtazak <murtazakasmanis@gmail.com>
2024-05-17 00:02:53 +0500

    login button add
    login button add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,23 +17,8 @@
     <header id="header" class="header d-flex align-items-center fixed-top">
         <div class="container-fluid container-xl position-relative d-flex align-items-center">
 
-            <a href="index.html" class="logo d-flex align-items-center me-auto">
-{{--                <img src="assets/img/logo.png" alt="">--}}
-                <h1 class="sitename">QuickStart</h1>
-            </a>
 
-            <nav id="navmenu" class="navmenu">
-                <ul>
-
-
-
-                    </li>
-
-                </ul>
-                <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-            </nav>
-
-            <a class="btn-getstarted" href="index.html#about">Login</a>
+            <a class="btn-getstarted" href="{{ route('login') }}">Login</a>
 
         </div>
     </header>
