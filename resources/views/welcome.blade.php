42e7925303fd14d851cfc30c62d5c36b79c26825
Gmurtazak <murtazakasmanis@gmail.com>
2024-05-17 03:22:37 +0500

    update main file
    update main file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -100,7 +100,7 @@
                         </div><!-- End Tab Content Item -->
 
                         <div class="tab-pane fade" id="features-tab-2">
-                            <img src="{{ asset('assets/images/tabs-2.jpgp') }}" alt="" class="img-fluid">
+                            <img src="{{ asset('assets/images/tabs-2.jpg') }}" alt="" class="img-fluid">
                         </div><!-- End Tab Content Item -->
 
                         <div class="tab-pane fade" id="features-tab-3">
@@ -115,6 +115,59 @@
         </div>
 
     </section><!-- /Features Section -->
+    <!-- Features Details Section -->
+    <section id="features-details" class="features-details section">
+
+        <div class="container">
+
+            <div class="row gy-4 justify-content-between features-item">
+
+                <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+                    <img src="{{ asset('assets/images/features-1.jpg') }}" class="img-fluid" alt="">
+                </div>
+
+                <div class="col-lg-5 d-flex align-items-center" data-aos="fade-up" data-aos-delay="200">
+                    <div class="content">
+                        <h3>Corporis temporibus maiores provident</h3>
+                        <p>
+                            Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
+                        </p>
+                        <a href="#" class="btn more-btn">Learn More</a>
+                    </div>
+                </div>
+
+            </div><!-- Features Item -->
+
+            <div class="row gy-4 justify-content-between features-item">
+
+                <div class="col-lg-5 d-flex align-items-center order-2 order-lg-1" data-aos="fade-up" data-aos-delay="100">
+
+                    <div class="content">
+                        <h3>Neque ipsum omnis sapiente quod quia dicta</h3>
+                        <p>
+                            Quidem qui dolore incidunt aut. In assumenda harum id iusto lorena plasico mares
+                        </p>
+                        <ul>
+                            <li><i class="bi bi-easel flex-shrink-0"></i> Et corporis ea eveniet ducimus.</li>
+                            <li><i class="bi bi-patch-check flex-shrink-0"></i> Exercitationem dolorem sapiente.</li>
+                            <li><i class="bi bi-brightness-high flex-shrink-0"></i> Veniam quia modi magnam.</li>
+                        </ul>
+                        <p></p>
+                        <a href="#" class="btn more-btn">Learn More</a>
+                    </div>
+
+                </div>
+
+                <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-up" data-aos-delay="200">
+                    <img src="{{ asset('assets/images/features-2.jpg') }}" class="img-fluid" alt="">
+                </div>
+
+            </div><!-- Features Item -->
+
+        </div>
+
+    </section><!-- /Features Details Section -->
     <!-- end wrapper-page -->
 @endsection
 
