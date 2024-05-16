b420c813d3860aab5bd84d7c56dd72ae6350b8bc
Gmurtazak <murtazakasmanis@gmail.com>
2024-05-16 22:46:43 +0500

    update page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,27 +1,22 @@
-@include('layouts.welcome')
+@extends('layouts.master-blank')
 
-    <div class="flex-center position-ref full-height">
-        @if (Route::has('login'))
-        <div class="top-right links color-white">
-            @auth
-            <a href="{{ url('/admin') }}">Admin</a>
-            @else
-            <a style="color: white" href="{{ route('login') }}">Login</a>
+@section('content')
 
-            @if (Route::has('register'))
-            <a href="{{ route('register') }}">Register</a>
-            @endif
-            @endauth
-        </div>
-        @endif
+    <div class="wrapper-page">
+        <div class="card overflow-hidden account-card mx-4 shadow">
 
-            <div class="wrapper-page">
-                <div class="card overflow-hidden account-card mx-4 shadow">
-
-
-                </div>
-
-
+            <div class="account-card-content">
+                <img src="{{ asset('assets/images/opalpayfull.png') }}" alt="Logo" class="img-fluid">
+             
             </div>
+        </div>
+
+
     </div>
+    <!-- end wrapper-page -->
+@endsection
+
+@section('script')
+@endsection
+
 
