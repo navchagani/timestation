74f32575823cc5ae1da8d81d6b199b2f28cf5605
Gmurtazak <murtazakasmanis@gmail.com>
2024-05-28 01:20:21 +0500

    update
    update

--- resources/views/admin/employeeinactive.blade.php
+++ resources/views/admin/employeeinactive.blade.php
@@ -7,56 +7,58 @@
         </div>
         <div class="card-body">
             <div class="form-group row">
-            <form>
-                <div class="col-md-12">
-                    <label class="required" for="employee">Select Reports</label>
-                    <select class="form-control" name="employeereport" id="employeereport">
-                        <option hidden>Please Select</option>
-                        <option value="/administrator-list">Administrator list</option>
-                        <option value="/attendance-counter">Attendance Counter</option>
-                        <option value="/attendance-list">Attendance Only</option>
-                        <option value="/department-list">Department list</option>
-                        <option value="/department-member">Department Members</option>
-                        <option value="/department-summary">Department Summary</option>
-                        <option value="/employee-summary">Employee Summary</option>
-                        <option value="/inactive-employee">Inactive Employee </option>
-                        <option value="/employee-permission">Employee Permission</option>
-                        <option value="/sheet-report">Employee Report</option>
-                        <option value="/current-employee">Current Employee Report</option>
-                        <option value="/employee-daily">Employee Daily Summary</option>
-                        <option value="/daily-absence">Employee Daily & Absence Report</option>
-                        <option value="/summary-reporttwo">Multiple Employee Summary Report</option>
-                    </select>
-                </div>
-                <div class="col-md-12">
+                <form method="POST" action="{{ route('employeesummaryfilters') }}">
+                    @csrf
+                    <div class="form-group row">
+                        <div class="col-md-2">
+                            <label class="required" for="employee">Select Reports</label>
+                            <select class="form-control" name="employeereport" id="employeereport">
+                                <option hidden>Please Select</option>
+                                <option value="/administrator-list">Administrator list</option>
+                                <option value="/attendance-counter">Attendance Counter</option>
+                                <option value="/attendance-list">Attendance Only</option>
+                                <option value="/department-list">Department list</option>
+                                <option value="/department-member">Department Members</option>
+                                <option value="/department-summary">Department Summary</option>
+                                <option value="/employee-summary">Employee Summary</option>
+                                <option value="/inactive-employee">Inactive Employee </option>
+                                <option value="/employee-permission">Employee Permission</option>
+                                <option value="/sheet-report">Employee Report</option>
+                                <option value="/current-employee">Current Employee Report</option>
+                                <option value="/employee-daily">Employee Daily Summary</option>
+                                <option value="/daily-absence">Employee Daily & Absence Report</option>
+                                <option value="/summary-reporttwo">Multiple Employee Summary Report</option>
+                            </select>
+                        </div>
+                        <div class="col-md-2">
 
-                    <label class="required" for="employee">Select Employee:</label>
-                    <select class="form-control" name="employee">
-                        <option hidden>Select an employee</option>
-                     {{--   @foreach($employeesa as $employee)
-                            <option value="{{ $employee->id }}" {{ $empid == $employee->id  ? 'selected' : '' }}>{{ $employee->name }}</option>
-                        @endforeach--}}
-                    </select>
-                </div>
-                <div class="col-md-2">
-                    <label for="time_in" class="col-sm-6 control-label">Start Date</label>
-                    <div class="bootstrap-timepicker">
-                        <input type="date" class="form-control timepicker" id="start" name="start" value="{{$start}}" required>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <label for="time_out" class="col-sm-6 control-label">End Date</label>
-                    <div class="bootstrap-timepicker">
-                        <input type="date" class="form-control timepicker" id="end" name="end" value="{{$end}}" required>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <label for="time_out" class="col-sm-6 control-label"><br></label>
-                    <button type="submit" class="btn btn-primary form-control">
-                        Run Report
-                    </button>
-                </div>
-            </form>
+                            <label class="required" for="employee">Select Employee:</label>
+                            <select class="form-control" name="employee">
+                                <option hidden>Select an employee</option>
+                                @foreach($employeesa as $employee)
+                                    <option value="{{ $employee->id }}" {{ $empid == $employee->id  ? 'selected' : '' }}>{{ $employee->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="col-md-2">
+                            <label for="time_in" class="col-sm-6 control-label">Start Date</label>
+                            <div class="bootstrap-timepicker">
+                                <input type="date" class="form-control timepicker" id="start" name="start" value="{{$start}}" required>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <label for="time_out" class="col-sm-6 control-label">End Date</label>
+                            <div class="bootstrap-timepicker">
+                                <input type="date" class="form-control timepicker" id="end" name="end" value="{{$end}}" required>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <label for="time_out" class="col-sm-6 control-label"><br></label>
+                            <button type="submit" class="btn btn-primary form-control">
+                                Run Report
+                            </button>
+                        </div>
+                </form>
             <div class="table-responsive">
                 <table id="datatable-buttons" class="table table-striped table-hover dt-responsive display nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                     <thead>
